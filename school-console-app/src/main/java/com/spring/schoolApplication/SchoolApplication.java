393a9a7ef684d91f7844740a16b8d5727801599e
Mykhailo Kornukh <mykhailo.kornukh@gmail.com>
2024-02-22 15:50:46 +0000

    Replace SchoolApplication.java

--- school-console-app/src/main/java/com/spring/schoolApplication/SchoolApplication.java
+++ school-console-app/src/main/java/com/spring/schoolApplication/SchoolApplication.java
@@ -1,20 +1,38 @@
 package com.spring.schoolApplication;
+
+import com.spring.schoolApplication.service.GeneratorService;
+import com.spring.schoolApplication.service.serviceImpl.GeneratorServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class SchoolApplication implements ApplicationRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SchoolApplication.class, args);
-	}
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private DataGeneratorUtil generatorRepository;
+    private DatabaseChecker checker;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolApplication.class, args);
+    }
 
 
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-
-	}
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        checker = new DatabaseChecker(jdbcTemplate);
+        if (checker.checkDatabaseOnEmpty() == 0) {
+            GeneratorService generatorService = new GeneratorServiceImpl(generatorRepository);
+            generatorService.addGeneratedCoursesToDatabase();
+            generatorService.addGeneratedGroupsToDatabase();
+            generatorService.addGeneratedStudentsToDatabase();
+            generatorService.setRandomCoursesForStudents();
+        }
+    }
 }
